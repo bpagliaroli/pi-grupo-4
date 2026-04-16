c27e2e8598b4ecdf3c1b1644a1db2ae338e87845
serenayaguee <serenayague527@gmail.com>
2026-04-16 17:19:45 -0300

    Agregue y termine lo de eliminar favoritos

--- src/Screens/Favoritos/Favoritos.js
+++ src/Screens/Favoritos/Favoritos.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import Cookies from "universal-cookie";
+const cookies = new Cookies();
 
 class Favoritos extends Component {
     constructor(props){
@@ -10,6 +13,10 @@ class Favoritos extends Component {
     }
 
     componentDidMount(){
+        if(!cookies.get("user-auth-cookie")){
+    this.props.history.push("/login");
+    return;
+}
         let favs = localStorage.getItem("favoritos");
 
         if(favs !== null){
@@ -21,25 +28,61 @@ class Favoritos extends Component {
             this.setState({
                 peliculas: pelis,
                 series: series
+
             });
         }
     }
 
-    render(){
-        return(
-            <div>
-                <h2>Películas favoritas</h2>
-                {this.state.peliculas.map(p => (
-                    <p key={p.id}>{p.nombre}</p>
-                ))}
+    eliminarFavorito = (id) => {
+    let favs = JSON.parse(localStorage.getItem("favoritos"));
 
-                <h2>Series favoritas</h2>
-                {this.state.series.map(s => (
-                    <p key={s.id}>{s.nombre}</p>
-                ))}
-            </div>
-        )
-    }
+    let filtrados = favs.filter(elem => elem.id !== id);
+
+    localStorage.setItem("favoritos", JSON.stringify(filtrados));
+
+    let pelis = filtrados.filter(e => e.tipo === "pelicula");
+    let series = filtrados.filter(e => e.tipo === "serie");
+
+    this.setState({
+        peliculas: pelis,
+        series: series
+    });
+}
+
+
+render(){
+    return(
+        <div>
+
+            <h2>Películas favoritas</h2>
+
+            {this.state.peliculas.map(p => (
+                <div key={p.id}>
+                    <Link to={`/detalle/${p.id}`}>
+                        <p>{p.title}</p>
+                    </Link>
+                    <button onClick={() => this.eliminarFavorito(p.id)}>
+                        Eliminar
+                    </button>
+                </div>
+            ))}
+
+            <h2>Series favoritas</h2>
+
+            {this.state.series.map(s => (
+                <div key={s.id}>
+                    <Link to={`/detalle/${s.id}`}>
+                        <p>{s.name}</p>
+                    </Link>
+                    <button onClick={() => this.eliminarFavorito(s.id)}>
+                        Eliminar
+                    </button>
+                </div>
+            ))}
+
+        </div>
+    )
+}
 }
 
 export default Favoritos;
